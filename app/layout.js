02cc3ea8ae964ef65dd89f8ff9971ba8db3a16bb
unknown <somegetiye@gmail.com>
2026-04-17 15:46:22 -0700

    adding font to the app

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,9 @@
 import Logo from "@/app/_components/Logo";
 import Nav from "./_components/Nav";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
 
 export const metadata = {
-  // title: "the-wild-oasis",
-
   title: {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
@@ -12,11 +11,17 @@ export const metadata = {
   discription:
     "Luxurious cabin hotel, located in the heart of the Italian Dolomites surrounded by beautiful mountains and dark forests",
 };
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 min-h-screen text-primary-50">
+      <body
+        className={`${josefin.className} bg-primary-950 min-h-screen text-primary-50`}
+      >
         <header>
           <Logo />
           <Nav />
