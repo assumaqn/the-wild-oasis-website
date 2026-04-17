458780b1be836dcd4b42502ea59cfd60f2931f1f
unknown <somegetiye@gmail.com>
2026-04-17 15:01:23 -0700

    styling the body

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Logo from "@/app/_components/Logo";
 import Nav from "./_components/Nav";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "the-wild-oasis",
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 min-h-screen text-primary-50">
         <header>
           <Logo />
           <Nav />
